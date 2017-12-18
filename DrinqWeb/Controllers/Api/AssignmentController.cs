99406fc60f02557d44554fb32a485a4522db8a15
m-rustam1996 <m-rustam1996@rambler.ru>
2017-12-18 22:38:58 +0800

    added return info about current assignment

---
+++ DrinqWeb/Controllers/Api/AssignmentController.cs
@@ -0,0 +1,40 @@
+﻿using DrinqWeb.Models;
+using DrinqWeb.Models.CodeFirstModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DrinqWeb.Controllers.Api
+{
+    public class AssignmentController : ApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get()
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            ApplicationUser user = Utils.GetUserById(Utils.GetAuthorizationStringFromHeader(Request));
+            if (user == null)
+                return Unauthorized();
+
+            UserAssignment userAssignment = db.UserAssignments.Include("Assignment")
+                .Where(item => item.UserId == user.Id &&
+                    item.Status == UserAssignmentStatus.InProgress)
+                .FirstOrDefault();
+
+            if (userAssignment == null)
+                return BadRequest("У Вас нет активных заданий.");
+
+            UserAssignment userAssignmentViewModel = new UserAssignment();
+            userAssignmentViewModel.Assignment = new Assignment();
+            userAssignmentViewModel.Assignment.Description = userAssignment.Assignment.Description;
+            userAssignmentViewModel.Status = userAssignment.Status;
+
+            var json = JsonConvert.SerializeObject(userAssignmentViewModel);
+            return Ok(json);
+        }
+    }
+}
