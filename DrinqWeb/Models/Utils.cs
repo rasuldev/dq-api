3ac57b990b940b5c7813a03d905d88ad6ecafce9
m-rustam1996 <m-rustam1996@rambler.ru>
2017-12-18 22:14:58 +0800

    add global functions

---
+++ DrinqWeb/Models/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace DrinqWeb.Models
+{
+    public class Utils
+    {
+        public static string GetAuthorizationStringFromHeader(HttpRequestMessage req)
+        {
+            return req.Headers.GetValues("Authorization").ToList().Count > 0 ?
+                req.Headers.GetValues("Authorization").ToList()[0] : null;
+        }
+
+
+        public static ApplicationUser GetUserById(string Id)
+        {
+            if (Id == null)
+                return null;
+            if (Id.Length < 3)
+                return null;
+            using (ApplicationDbContext db = new ApplicationDbContext())
+                return db.Users.Where(e => e.Id == Id).FirstOrDefault(); ;
+        }
+
+    }
+}
