cd8a334de9d2b8b5645bcde2ae620723d7b8f6c3
m-rustam1996 <m-rustam1996@rambler.ru>
2017-12-19 15:53:47 +0800

    added checkText method

--- DrinqWeb/Controllers/Api/AssignmentController.cs
+++ DrinqWeb/Controllers/Api/AssignmentController.cs
@@ -20,21 +20,159 @@ namespace DrinqWeb.Controllers.Api
             if (user == null)
                 return Unauthorized();
 
-            UserAssignment userAssignment = db.UserAssignments.Include("Assignment")
-                .Where(item => item.UserId == user.Id &&
-                    item.Status == UserAssignmentStatus.InProgress)
-                .FirstOrDefault();
+            UserAssignment userAssignment = GetCurrentUserAssignment(db, user.Id);
 
             if (userAssignment == null)
                 return BadRequest("У Вас нет активных заданий.");
-
-            UserAssignment userAssignmentViewModel = new UserAssignment();
-            userAssignmentViewModel.Assignment = new Assignment();
-            userAssignmentViewModel.Assignment.Description = userAssignment.Assignment.Description;
-            userAssignmentViewModel.Status = userAssignment.Status;
-
-            var json = JsonConvert.SerializeObject(userAssignmentViewModel);
+            AssignmentResponseModel assignmentResponseModel = new AssignmentResponseModel(Status.Accepted, userAssignment.Assignment);
+            assignmentResponseModel.Assignment.TextCodes = null;
+            assignmentResponseModel.Assignment.Quest = null;
+            var json = JsonConvert.SerializeObject(assignmentResponseModel);
             return Ok(json);
+        }
+
+        private UserAssignment GetCurrentUserAssignment(ApplicationDbContext db, string userId)
+        {
+            return db.UserAssignments.Include("Assignment.Quest").Include("UserQuest")
+                .Where(item => item.UserId == userId &&
+                    item.Status == UserAssignmentStatus.InProgress)
+                .FirstOrDefault();
+        }
+
+
+        private UserAssignment GetNextUserAssignment(UserAssignment currentUserAssignment, ApplicationDbContext db)
+        {
+            var nextAssignmentSortValue = currentUserAssignment.Assignment.Sort + 1;
+            return db.UserAssignments.Include("Assignment")
+                .Where(item =>
+                    item.UserId == currentUserAssignment.UserId &&
+                    item.Status == UserAssignmentStatus.NotAvailable &&
+                    item.Assignment.Sort == nextAssignmentSortValue)
+                .FirstOrDefault();
+        }
+
+        public enum Status
+        {
+            Wrong, Accepted, Completed
+        }
+
+        [HttpPost]
+        public IHttpActionResult CheckText([FromBody] string codes)
+        {
+            ApplicationDbContext db = new ApplicationDbContext();
+            ApplicationUser user = Utils.GetUserById(Utils.GetAuthorizationStringFromHeader(Request));
+            if (user == null)
+                return Unauthorized();
+
+            var currentUserAssignment = GetCurrentUserAssignment(db, user.Id);
+            if (currentUserAssignment == null)
+                return BadRequest("У Вас нет активного задания.");
+
+            // Check user quest duration
+            int currentUserAssignmentDuration = (DateTime.Now - currentUserAssignment.UserQuest.StartDate).Minutes;
+            if (currentUserAssignmentDuration > currentUserAssignment.UserQuest.Quest.MaxTime)
+            {
+                DismissCurrentQuest(db, currentUserAssignment.UserQuest);
+                return Ok("Quest time is expired.");
+            }
+            // -- Check user quest duration
+
+            var jsonResponse = "";
+            var status = Status.Accepted;
+
+            // Check answers
+            string[] userCodes = codes.Split(new string[] { "|$|" }, StringSplitOptions.RemoveEmptyEntries);
+            string[] correctCodes = currentUserAssignment.Assignment.TextCodes.Split(new string[] { "|$|" }, StringSplitOptions.RemoveEmptyEntries);
+            if (userCodes.Length == correctCodes.Length)
+                for (int i = 0; i < userCodes.Length; i++)
+                {
+                    if (userCodes[i] != correctCodes[i])
+                    {
+                        status = Status.Wrong;
+                        break;
+                    }
+                }
+            else
+                status = Status.Wrong;
+            // -- Check answers
+
+            // Get next assignment
+            UserAssignment nextUserAssignment = null;
+            Assignment responseNextAssignment = null;
+            switch (status)
+            {
+                case Status.Accepted:
+                    currentUserAssignment.TextCodeAccepted = UserAssignmentAcceptedStatus.Accepted;
+                    currentUserAssignment.Status = UserAssignmentStatus.Completed;
+                    currentUserAssignment.EndDate = DateTime.Now;
+                    // check media (not alpha)
+                    status = Status.Completed;
+                    nextUserAssignment = GetNextUserAssignment(currentUserAssignment, db);
+                    if (nextUserAssignment == null)
+                    {
+                        currentUserAssignment.UserQuest.Status = UserQuestStatus.Completed;
+                        responseNextAssignment = null;
+                    }
+                    else
+                    {
+                        nextUserAssignment.Status = UserAssignmentStatus.InProgress;
+                        nextUserAssignment.StartDate = DateTime.Now;
+                        responseNextAssignment = nextUserAssignment.Assignment;
+                    }
+                    break;
+                case Status.Wrong:
+                    currentUserAssignment.TextCodeAccepted = UserAssignmentAcceptedStatus.Declined;
+                    break;
+            }
+            // -- Get next assignment
+            db.SaveChanges();
+
+            // RESPONSE
+            responseNextAssignment.TextCodes = null;
+            jsonResponse = JsonConvert.SerializeObject(new AssignmentResponseModel(status, responseNextAssignment), Formatting.Indented, new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            return Ok(jsonResponse);
+            // CUQD - Check user quest duration
+            //      WRONG -> set quest status to Failed.
+            //               SaveChanges.
+            //               Return.
+
+            //  Check user
+            //  Check currentAssignment
+            //  CUQD
+            //  var jsonStr
+            //  Compare code and correct text code:
+            //      OK->    currentAssignment.TextAccepted = Accepted;
+            //              currentAssignment.Status = Completed;
+            //              status = Completed;
+            //              Get next assignment:
+            //                  OK ->   assignment.Status = InProgress;
+            //                  NULL -> currentAssignment.Quest = Completed;
+            //              jsonStr = status + assignment(may be null);
+            //      WRONG-> currentAssignment.TextAccepted = Declined;
+            //              status = Declined;
+            //  return jsonStr;
+        }
+
+        public void DismissCurrentQuest(ApplicationDbContext db, UserQuest userQuest)
+        {
+            var userAssignments = db.UserAssignments.Where(item =>
+                item.UserQuest.Id == userQuest.Id &&
+                item.UserId == userQuest.UserId &&
+                item.Status != UserAssignmentStatus.Completed &&
+                item.Status != UserAssignmentStatus.Failed).ToList();
+            foreach (var assignment in userAssignments)
+            {
+                assignment.Status = UserAssignmentStatus.Failed;
+                db.Entry(assignment).State = System.Data.Entity.EntityState.Modified;
+            }
+
+            userQuest.Status = UserQuestStatus.Failed;
+            db.Entry(userQuest).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
         }
     }
 }
