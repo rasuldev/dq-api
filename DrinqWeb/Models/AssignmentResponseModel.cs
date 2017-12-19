75a6ec6a2b727fee29262967c93642c77ac606e2
m-rustam1996 <m-rustam1996@rambler.ru>
2017-12-19 15:52:33 +0800

    added model for response for assignment controller

---
+++ DrinqWeb/Models/AssignmentResponseModel.cs
@@ -0,0 +1,20 @@
+﻿using DrinqWeb.Models.CodeFirstModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrinqWeb.Models
+{
+    public class AssignmentResponseModel
+    {
+        public Controllers.Api.AssignmentController.Status Status { get; set; }
+        public Assignment Assignment { get; set; }
+
+        public AssignmentResponseModel(Controllers.Api.AssignmentController.Status status, Assignment assignment)
+        {
+            Status = status;
+            Assignment = assignment;
+        }
+    }
+}
