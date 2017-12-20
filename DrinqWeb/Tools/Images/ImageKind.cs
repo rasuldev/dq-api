c58426433010c4ed91433052c03ce68300065c5c
m-rustam1996 <m-rustam1996@rambler.ru>
2017-12-20 16:28:11 +0800

    added imageFactory for image upload

---
+++ DrinqWeb/Tools/Images/ImageKind.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrinqWeb.Tools.Images
+{
+    public enum ImageKind
+    {
+        VerificationItem
+    }
+}
