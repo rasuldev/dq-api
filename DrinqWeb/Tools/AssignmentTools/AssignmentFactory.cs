00f7eeb8c1105df6ed1c14de4ead292f82bb092b
m-rustam1996 <m-rustam1996@rambler.ru>
2017-12-21 01:42:52 +0800

    create assignmentTools class

---
+++ DrinqWeb/Tools/AssignmentTools/AssignmentFactory.cs
@@ -0,0 +1,38 @@
+﻿using DrinqWeb.Models;
+using DrinqWeb.Models.CodeFirstModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrinqWeb.Tools.AssignmentTools
+{
+    public class AssignmentFactory
+    {
+
+        public AssignmentFactory()
+        {
+
+        }
+
+        public UserAssignment GetCurrentUserAssignment(ApplicationDbContext db, string userId)
+        {
+            return db.UserAssignments.Include("Assignment.Quest").Include("UserQuest")
+                .Where(item => item.UserId == userId &&
+                    item.Status == UserAssignmentStatus.InProgress)
+                .FirstOrDefault();
+        }
+
+
+        public UserAssignment GetNextUserAssignment(UserAssignment currentUserAssignment, ApplicationDbContext db)
+        {
+            var nextAssignmentSortValue = currentUserAssignment.Assignment.Sort + 1;
+            return db.UserAssignments.Include("Assignment")
+                .Where(item =>
+                    item.UserId == currentUserAssignment.UserId &&
+                    item.Status == UserAssignmentStatus.NotAvailable &&
+                    item.Assignment.Sort == nextAssignmentSortValue)
+                .FirstOrDefault();
+        }
+    }
+}
