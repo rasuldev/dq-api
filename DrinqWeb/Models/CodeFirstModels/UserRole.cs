a6e01aca292740458d82517b71d6485c0c47eaa0
m-rustam1996 <m-rustam1996@rambler.ru>
2017-12-22 17:25:05 +0800

    added roles

---
+++ DrinqWeb/Models/CodeFirstModels/UserRole.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DrinqWeb.Models.CodeFirstModels
+{
+    public enum UserRole
+    {
+        Admin, Appraiser, Player
+    }
+}
